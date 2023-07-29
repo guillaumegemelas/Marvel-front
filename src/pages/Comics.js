ad12006555154893268cc00d95974a48d24a2131
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2023-07-30 00:34:01 +0200

    test usequery

--- src/pages/Comics.js
+++ src/pages/Comics.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import ComicCard from "../Components/ComicCard";
 // import bandeauCom from "../img/bandeauCom.png";
 
@@ -14,26 +14,33 @@ const Comics = ({ token }) => {
     window.scrollTo(0, 0);
   }, []);
 
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await axios.get(
+        `https://site--marvel-back--zqfvjrr4byql.code.run/comics?apiKey=&skip=${skip}&title=${title}`
+      );
+      setComics(response.data);
+      setIsloading(false);
+    } catch (error) {
+      console.log(error.message);
+      console.log(error.response);
+    }
+  }, [skip, title]);
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `https://site--marvel-back--zqfvjrr4byql.code.run/comics?apiKey=&skip=${skip}&title=${title}`
-        );
-        setComics(response.data);
-        setIsloading(false);
-      } catch (error) {
-        console.log(error.message);
-        console.log(error.response);
-      }
-    };
     fetchData();
-  }, [title, skip]);
+  }, [fetchData]);
 
   return (
     <div>
       {/* <img className="bandeau" src={bandeauCom} alt="" /> */}
       <div className="searchBar">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            fetchData();
+          }}
+        ></form>
         <input
           className="search"
           type="text"
@@ -50,6 +57,7 @@ const Comics = ({ token }) => {
           placeholder="Page"
           onChange={(event) => setSkip(event.target.value)}
         />
+        <button type="submit">Rechercher</button>
       </div>
 
       {isLoading ? (
